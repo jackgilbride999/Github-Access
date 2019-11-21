477a05fc096f8afbff5c11ab0f9193e393f767a9
Jack Gilbride <jackgilbride999@gmail.com>
2019-11-21 22:47:34 +0000

    Started Social Graph branch. Current implementation prints information about a user's repositories, as well as ones belonging to those they are following

--- Workspace/Github-Access/src/Collector.java
+++ Workspace/Github-Access/src/Collector.java
@@ -7,38 +7,44 @@ import org.eclipse.egit.github.core.service.*;
 public class Collector {
 
 	public static void main(String[] args) {
-		while (true) {
-			Scanner scanner = new Scanner(System.in);
-			try {
-				GitHubClient client = new GitHubClient();
-				System.out.println("Please enter your Github username");
-				String username = scanner.nextLine();
-				System.out.println("Welcome, " + username + "! Please enter your Github password");
-				String password = scanner.nextLine();
-				client.setCredentials(username, password);
-				RepositoryService repoService = new RepositoryService();
-				while (true) {
-					System.out.println(
-							"Please enter a username to view their repository information, or type 'quit' to exit.");
-					username = scanner.nextLine();
-					if (username.equalsIgnoreCase("quit"))
-						return;
-					for (Repository repo : repoService.getRepositories(username)) {
-						System.out.println("Repository name: " + repo.getName());
-						System.out.println("	- Language: " + repo.getLanguage());
-						System.out.println("	- Description: " + repo.getDescription());
-						System.out.println("	- Size: " + repo.getSize() + " kB");
-						System.out.println("	- Number of watchers: " + repo.getWatchers());
-						System.out.println("	- Number of forks: " + repo.getForks());
-						System.out.println("	- Created at: " + repo.getCreatedAt());
-						System.out.println("	- Updated at: " + repo.getUpdatedAt());
-						System.out.println("	- URL: " + repo.getGitUrl());
+		Scanner scanner = new Scanner(System.in);
+		try {
+			GitHubClient client = new GitHubClient();
+			System.out.println("Please enter your Github username");
+			String username = scanner.nextLine();
+			System.out.println("Welcome, " + username + "! Please enter your Github password");
+			String password = scanner.nextLine();
+			client.setCredentials(username, password);
+			RepositoryService repoService = new RepositoryService();
+			UserService userService = new UserService(client);
+			while (true) {
+				System.out.println(
+						"Please enter a username to view information about a user and who they are following, or type 'quit' to exit.");
+				username = scanner.nextLine();
+				if (username.equalsIgnoreCase("quit"))
+					return;
+				for (User user : userService.getFollowing()) {
+					System.out.println(user.getLogin());
+					System.out.println("	Follower count:" + user.getFollowers());
+					System.out.println("	Following count:" + user.getFollowers());
+					for (Repository repo : repoService.getRepositories(user.getLogin())) {
+						System.out.println("	Repository name: " + repo.getName());
+						System.out.println("		- Language: " + repo.getLanguage());
+						System.out.println("		- Description: " + repo.getDescription());
+						System.out.println("		- Size: " + repo.getSize() + " kB");
+						System.out.println("		- Number of watchers: " + repo.getWatchers());
+						System.out.println("		- Number of forks: " + repo.getForks());
+						System.out.println("		- Created at: " + repo.getCreatedAt());
+						System.out.println("		- Updated at: " + repo.getUpdatedAt());
+						System.out.println("		- URL: " + repo.getGitUrl());
+						System.out.println("		- Commit comments: ");
 					}
 				}
-			} catch (Exception e) {
-				System.out.println("Sorry, an unexpected error has occured.");
 			}
-			scanner.close();
+		} catch (Exception e) {
+			System.out.println("Sorry, an unexpected error has occured.");
+			e.printStackTrace();
 		}
+		scanner.close();
 	}
 }
