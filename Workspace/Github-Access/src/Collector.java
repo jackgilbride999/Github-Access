b71d936194c0fb931ed33d8810d4471d4748919a
Jack Gilbride <jackgilbride999@gmail.com>
2019-11-22 16:16:49 +0000

    Refined code use case. The program will interrogate the API for data about those who the signed in user is following

--- Workspace/Github-Access/src/Collector.java
+++ Workspace/Github-Access/src/Collector.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.Scanner;
 
 import org.eclipse.egit.github.core.*;
@@ -10,6 +11,7 @@ public class Collector {
 		Scanner scanner = new Scanner(System.in);
 		try {
 			GitHubClient client = new GitHubClient();
+			System.out.println("Welcome to Github Access. This program interrogates the Github API for data about those you are following.");
 			System.out.println("Please enter your Github username");
 			String username = scanner.nextLine();
 			System.out.println("Welcome, " + username + "! Please enter your Github password");
@@ -17,28 +19,23 @@ public class Collector {
 			client.setCredentials(username, password);
 			RepositoryService repoService = new RepositoryService();
 			UserService userService = new UserService(client);
-			while (true) {
-				System.out.println(
-						"Please enter a username to view information about a user and who they are following, or type 'quit' to exit.");
-				username = scanner.nextLine();
-				if (username.equalsIgnoreCase("quit"))
-					return;
-				for (User user : userService.getFollowing()) {
-					System.out.println(user.getLogin());
-					System.out.println("	Follower count:" + user.getFollowers());
-					System.out.println("	Following count:" + user.getFollowers());
-					for (Repository repo : repoService.getRepositories(user.getLogin())) {
-						System.out.println("	Repository name: " + repo.getName());
-						System.out.println("		- Language: " + repo.getLanguage());
-						System.out.println("		- Description: " + repo.getDescription());
-						System.out.println("		- Size: " + repo.getSize() + " kB");
-						System.out.println("		- Number of watchers: " + repo.getWatchers());
-						System.out.println("		- Number of forks: " + repo.getForks());
-						System.out.println("		- Created at: " + repo.getCreatedAt());
-						System.out.println("		- Updated at: " + repo.getUpdatedAt());
-						System.out.println("		- URL: " + repo.getGitUrl());
-						System.out.println("		- Commit comments: ");
-					}
+			List<User> users = userService.getFollowing();
+			for (User user : users) {
+				System.out.println(user.getLogin());
+				System.out.println("	Follower count:" + user.getFollowers());
+				System.out.println("	Following count:" + user.getFollowers());
+				List<Repository> repos = repoService.getRepositories(user.getLogin();
+				for (Repository repo : repos) {
+					System.out.println("	Repository name: " + repo.getName());
+					System.out.println("		- Language: " + repo.getLanguage());
+					System.out.println("		- Description: " + repo.getDescription());
+					System.out.println("		- Size: " + repo.getSize() + " kB");
+					System.out.println("		- Number of watchers: " + repo.getWatchers());
+					System.out.println("		- Number of forks: " + repo.getForks());
+					System.out.println("		- Created at: " + repo.getCreatedAt());
+					System.out.println("		- Updated at: " + repo.getUpdatedAt());
+					System.out.println("		- URL: " + repo.getGitUrl());
+					System.out.println("		- Commit comments: ");
 				}
 			}
 		} catch (Exception e) {
