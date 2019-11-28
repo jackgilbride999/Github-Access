1a49900a454d4f7795ad81f626b94289229dd0bf
Jack Gilbride <jackgilbride999@gmail.com>
2019-11-28 15:53:31 +0000

    Connection with MongoDB established and Collector class expanded to hold Github objects

--- Workspace/Github-Access/src/Collector.java
+++ Workspace/Github-Access/src/Collector.java
@@ -1,16 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import org.eclipse.egit.github.core.*;
 import org.eclipse.egit.github.core.client.*;
 import org.eclipse.egit.github.core.service.*;
 
-import com.github.gwtd3.api.D3;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
 
 public class Collector {
+	
+	List<User> userList;
+	Map<User, List<Repository>> userRepositories;
+	
+	Collector(){
+		userList = new ArrayList<User>();
+		userRepositories = new HashMap<User, List<Repository>>();
+	}
+	
+	private void addUser(User user) {
+		userList.add(user);
+	}
+	
+	private void addRepository(User user, Repository repo) {
+		if(userRepositories.containsKey(user)) {
+			(userRepositories.get(user)).add(repo);
+		} else {
+			userRepositories.put(user, new ArrayList<Repository>());
+		}
+	}
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		MongoClientURI uri = new MongoClientURI(
+			    "mongodb+srv://jackgilbride999:73hog24Ghhdq6BWg@github-ueksk.mongodb.net/test?retryWrites=true&w=majority");
+			
+		MongoClient mongoClient = new MongoClient(uri);
+		MongoDatabase database = mongoClient.getDatabase("test");
+		System.out.println(database.toString());
 		try {
 			GitHubClient client = new GitHubClient();
 			System.out.println("Welcome to Github Access. This program interrogates the Github API for data about those you are following.");
@@ -21,7 +52,6 @@ public class Collector {
 			client.setCredentials(username, password);
 			RepositoryService repoService = new RepositoryService(client);
 			UserService userService = new UserService(client);
-			CommitService commitService = new CommitService(client);
 			List<User> users = userService.getFollowing();
 			for (User user : users) {
 				System.out.println(user.getLogin());
